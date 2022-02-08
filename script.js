7668fd5e0ee4e1298de31a111a43866c51f4c448
ianm93 <45110918+iannm93@users.noreply.github.com>
2022-02-08 02:53:52 -0800

    added like button and centered movie titles

--- script.js
+++ script.js
@@ -58,8 +58,11 @@ function popularMovies() {
         <div class="film">
         <img src="${"https://www.themoviedb.org/t/p/original" + response.results[i].poster_path}" alt="poster-image">
         <div class="film-info">
-            <h3>${response.results[i].title}</h3>
-            <span>${response.results[i].vote_average}</span>
+            <h3 class ="movie-title">${response.results[i].title}</h3>
+            <span class ="votes">${response.results[i].vote_average} </span>
+             <img class="like-button"src="https://cdn2.iconfinder.com/data/icons/media-player-ui/512/Media-Icon-25-512.png">
+
+            
         </div>
     </div>`
         // // create image element each iteration
