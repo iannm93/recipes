d1bdc4d40a6e0e7e5cbbac20f82eb3af3365172d
ianm93 <45110918+iannm93@users.noreply.github.com>
2022-04-20 05:44:10 -0700

    added prevention for if user enters strin t hat has no return from API. uses last search instead of depopulating all the html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let userSearches = [];
 // API key and query selectors to target later
 const API_KEY = "9236658842912fdafd6330610c3dcb5c";
 // use this key to use certain features of API
@@ -5,9 +6,7 @@ const API2 = "04c35731a5ee918f014970082a0088b1";
 const formValue = document.getElementById("form");
 const plot = document.getElementById("overview");
 const emptyDiv = document.getElementById("empty-div");
-
-
-
+const returnTo20 = document.getElementById("return");
 // const URL_API = "https://api.themoviedb.org/3/search/movie?api_key="+ API_KEY + "&language=en-US&page=1&include_adult=false&query=split"
 let searchMovie = document.querySelector("#search-movie");
 // waiting for a click on the search movie button
@@ -17,27 +16,24 @@ searchMovie.addEventListener("click", movieButton);
 function movieButton(event) {
   // prevent default stops the browser from automatically resetting the value of the form every time it is interacted with
   event.preventDefault();
-  
+
   // assign the value of the text input to variable
   let userInput = formValue.value;
-  if (userInput === ""){
-    confirm("Please enter a movie or TV show in order to search.")
-    
-  } else
-  emptyDiv.innerHTML = "";
+  if (userInput === "") {
+    confirm("Please enter a movie or TV show in order to search.");
+  } else emptyDiv.innerHTML = "";
   // pass whatever value was in the search bar to API function
   movieSearch(userInput);
 }
 
 // function to return  popular movies to append to the dom on load of the site
-$(document).ready(function(){
-
+$(document).ready(function () {
   let random = Math.floor(Math.random() * 999);
-  
+
   const URL_FOR_POPULAR =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" +
-  API2 +
-  "&page=1" 
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" +
+    API2 +
+    "&page=1";
   let settings = {
     async: true,
     crossDomain: true,
@@ -46,55 +42,57 @@ $(document).ready(function(){
   };
 
   $.ajax(settings).then((response) => {
-   
     // i < response.results.length VVVVVV
+    console.log(response);
     for (let i = 0; i < 18; i++) {
-      
       // create div element for top 20 movies and add class name
       const top20 = document.createElement("div");
       top20.className = "top20";
       // top20.style.display = "inline-block"
       // assign the innerHTML to put each film inside it's own container
       // populates with data from API
+
       top20.innerHTML = `
-        <div class="film">
-        <img src="${"https://www.themoviedb.org/t/p/original" + response.results[i].poster_path}" alt="poster-image">
+      <div class="film">
+      <img src="${
+        "https://www.themoviedb.org/t/p/original" +
+        response.results[i].poster_path
+      }" alt="poster-image">
         <div class="film-info">
-            <h4 class ="movie-title">${response.results[i].title}</h4>
-            
-            <img class="like-button"src="https://cdn2.iconfinder.com/data/icons/media-player-ui/512/Media-Icon-25-512.png">
-            <span class ="votes">${response.results[i].vote_average} </span>
-            
-            </div>
+        <h4 class ="movie-title">${response.results[i].title}</h4>
+        
+        <img class="like-button"src="https://cdn2.iconfinder.com/data/icons/media-player-ui/512/Media-Icon-25-512.png">
+        <span class ="votes">${response.results[i].vote_average} </span>
+        
+        </div>
             </div>`;
-            emptyDiv.append(top20);
+      emptyDiv.append(top20);
     }
-
-  })
-    
-    // for each method below
-    // response.results.forEach(film=>{
-    //     console.log(film)
-    //     // create element for top 20 posters
-    //     const postIMG = document.createElement("img")
-    //     // make the SRC of the image element just created url to film's poster
-    //     postIMG.src = "https://www.themoviedb.org/t/p/original" + film.poster_path
-    //     // give it an ID, height, width
-    //     postIMG.id = "top20"
-    //     postIMG.height = 400;
-    //     postIMG.width = 300;
-    //     // const voteAverage = document.createElement("p")
-    //     // console.log(film.vote_average)
-    //     // voteAverage.innerHTML = film.vote_average
-
-    //     emptyDiv.append(postIMG)
-    //     // emptyDiv.append(voteAverage)
-
-    //     console.log(postIMG)
-    // })
-
-    // loop through response array
   });
+
+  // for each method below
+  // response.results.forEach(film=>{
+  //     console.log(film)
+  //     // create element for top 20 posters
+  //     const postIMG = document.createElement("img")
+  //     // make the SRC of the image element just created url to film's poster
+  //     postIMG.src = "https://www.themoviedb.org/t/p/original" + film.poster_path
+  //     // give it an ID, height, width
+  //     postIMG.id = "top20"
+  //     postIMG.height = 400;
+  //     postIMG.width = 300;
+  //     // const voteAverage = document.createElement("p")
+  //     // console.log(film.vote_average)
+  //     // voteAverage.innerHTML = film.vote_average
+
+  //     emptyDiv.append(postIMG)
+  //     // emptyDiv.append(voteAverage)
+
+  //     console.log(postIMG)
+  // })
+
+  // loop through response array
+});
 
 // function popularMoviestwo() {
 //   let random = Math.floor(Math.random() * 999);
@@ -102,7 +100,7 @@ $(document).ready(function(){
 //   const URL_FOR_POPULAR =
 //   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" +
 //   API2 +
-//   "&page=2" 
+//   "&page=2"
 //   let settings = {
 //     async: true,
 //     crossDomain: true,
@@ -126,16 +124,15 @@ $(document).ready(function(){
 //         <img src="${"https://www.themoviedb.org/t/p/original" + response.results[i].poster_path}" alt="poster-image">
 //         <div class="film-info">
 //             <h4 class ="movie-title">${response.results[i].title}</h4>
-            
+
 //             <img class="like-button"src="https://cdn2.iconfinder.com/data/icons/media-player-ui/512/Media-Icon-25-512.png">
 //             <span class ="votes">${response.results[i].vote_average} </span>
-            
+
 //             </div>
 //     </div>`;
 //       emptyDiv.append(top40);
 //     }
 
-    
 //     // for each method below
 //     // response.results.forEach(film=>{
 //     //     console.log(film)
@@ -162,16 +159,18 @@ $(document).ready(function(){
 // }
 // popularMoviestwo();
 
-
 // popularMovies();
 
 function movieSearch(movie) {
   // API query parameters, concat the value of whatever was passed into this function to the end of params
+  // movie.preventDefault
+  userSearches.push(movie);
+  console.log(userSearches);
   const URL_API =
-    "https://api.themoviedb.org/3/search/movie?api_key=" +
-    API_KEY +
-    "&language=en-US&page=1&include_adult=true&query=" +
-    movie;
+  "https://api.themoviedb.org/3/search/movie?api_key=" +
+  API_KEY +
+  "&language=en-US&page=1&include_adult=true&query=" +
+  movie;
   
   var settings = {
     async: true,
@@ -180,34 +179,44 @@ function movieSearch(movie) {
     method: "GET",
     headers: {},
   };
-
+  
   $.ajax(settings)
-    .then((response) => {
-      // i< response.results.length VVV if needs to be changed
-      // because of page layout, made sense to cut it down to 18 rather than 
-      // the typical 20 of the response object as it fits evenly in more
-      // displays across mobile, ipad, desktop, laptop
-      for (let i = 0; i < 18; i++) {
-        // create div element for top 20 movies and add class name
-        const userSearch = document.createElement("div");
-        userSearch.className = "userSearch";
-        // userSearch.style.display = "inline-block"
-        // assign the innerHTML to put each film inside it's own container
-        // populates with data from API
+  .then((response) => {
+    // i< response.results.length VVV if needs to be changed
+    // because of page layout, made sense to cut it down to 18 rather than
+    // the typical 20 of the response object as it fits evenly in more
+    // displays across mobile, ipad, desktop, laptop
+    for (let i = 0; i < 18; i++) {
+      // create div element for top 20 movies and add class name
+      const userSearch = document.createElement("div");
+      userSearch.className = "userSearch";
+      // userSearch.style.display = "inline-block"
+      // assign the innerHTML to put each film inside it's own container
+      // populates with data from API
+      if (response.results.length != 0) {
         userSearch.innerHTML = `
-            <div class="film">
-            <img src="${
-              "https://www.themoviedb.org/t/p/original" +
-              response.results[i].poster_path}" alt="poster-image">
-            <div class="film-info">
-                <h4 class ="movie-title">${response.results[i].title}</h4>
-                <img class="like-button"src="https://cdn2.iconfinder.com/data/icons/media-player-ui/512/Media-Icon-25-512.png">
-                <span class ="votes">${response.results[i].vote_average} </span>
-    
-                
-            </div>
-        </div>`;
-        emptyDiv.append(userSearch);
+        <div class="film">
+        <img src="${
+          "https://www.themoviedb.org/t/p/original" +
+          response.results[i].poster_path
+        }" alt="poster-image">
+        <div class="film-info">
+        <h4 class ="movie-title">${response.results[i].title}</h4>
+        <img class="like-button"src="https://cdn2.iconfinder.com/data/icons/media-player-ui/512/Media-Icon-25-512.png">
+        <span class ="votes">${response.results[i].vote_average} </span>
+        
+        
+        </div>
+          </div>`;
+          emptyDiv.append(userSearch);
+        }
+      }
+      let last = userSearches.length - 2;
+      console.log(userSearches[last])
+      if (response.results.length === 0) {
+        
+        movieSearch(userSearches[last]);
+        return;
       }
     })
     .catch((error) => {
@@ -217,6 +226,7 @@ function movieSearch(movie) {
       );
     });
 }
+console.log(userSearches);
 array = [5, 2, 3];
 function sort(array) {
   for (i = 0; i < array.length; i++) {
@@ -233,3 +243,79 @@ function sort(array) {
   return array;
 }
 sort(array);
+
+// function to return  popular movies to append to the dom on load of the site
+// $(document).ready(function () {
+//   let token =
+//     "Bearer L5LP46_Y5o-XZR6vdKcPk0t6_gS6id7Kg8lfsBsWj1VVlhH9NeddjVJcCzfsfDm6kMJIN7K0viaoh2Nf7VcTRlmjOLmArJyHcsuJtf7bFsd2HG9gw10JcSqQKKdfYnYx";
+//   let cors = 'https://cors-anywhere.herokuapp.com'
+
+//   let yelp = "https://api.yelp.com/v3/businesses/law-office-of-scott-c-harris-rancho-santa-fe/reviews"
+
+//   let settings = {
+//     url: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/law-office-of-scott-c-harris-rancho-santa-fe/reviews",
+//     header:{'Authorization': token},
+//     method: "GET",
+//   };
+
+//   $.ajax(settings).then((response) => {
+//     console.log(response);
+//   });
+// });
+let token =
+  "Bearer L5LP46_Y5o-XZR6vdKcPk0t6_gS6id7Kg8lfsBsWj1VVlhH9NeddjVJcCzfsfDm6kMJIN7K0viaoh2Nf7VcTRlmjOLmArJyHcsuJtf7bFsd2HG9gw10JcSqQKKdfYnYx";
+let url =
+  "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/law-office-of-scott-c-harris-rancho-santa-fe";
+const options = {
+  method: "GET",
+  headers: {
+    Authorization: token,
+  },
+};
+fetch(url, options).then((res) => {
+  console.log(res);
+});
+
+function popularMovies() {
+  const URL_FOR_POPULAR =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" +
+    API2 +
+    "&page=1";
+  let settings = {
+    async: true,
+    crossDomain: true,
+    url: URL_FOR_POPULAR,
+    method: "GET",
+  };
+
+  $.ajax(settings).then((response) => {
+    // i < response.results.length VVVVVV
+    emptyDiv.innerHTML = "";
+    for (let i = 0; i < 18; i++) {
+      // create div element for top 20 movies and add class name
+      const top20 = document.createElement("div");
+      top20.className = "top20";
+      // top20.style.display = "inline-block"
+      // assign the innerHTML to put each film inside it's own container
+      // populates with data from API
+      top20.innerHTML = `
+            <div class="film">
+            <img src="${
+              "https://www.themoviedb.org/t/p/original" +
+              response.results[i].poster_path
+            }" alt="poster-image">
+            <div class="film-info">
+                <h4 class ="movie-title">${response.results[i].title}</h4>
+                
+                <img class="like-button"src="https://cdn2.iconfinder.com/data/icons/media-player-ui/512/Media-Icon-25-512.png">
+                <span class ="votes">${response.results[i].vote_average} </span>
+                
+                </div>
+                </div>`;
+      emptyDiv.append(top20);
+    }
+  });
+}
+returnTo20.addEventListener("click", function () {
+  popularMovies();
+});
